3f4ac88c0ff1302b8985bb76374ffd322fd7dc03
mariaion <marianaion319@gmail.com>
2023-11-20 10:21:57 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+namespace Begginer_LevelCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change the appearance 
+            Console.Title = "Skynet";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World");
+            Console.WindowHeight = 40;
+
+            //get a conversation going
+            Console.WriteLine("Hello, what's your name?"); 
+            Console.ReadLine();
+            Console.WriteLine("My name is rx-9000. I'm an AI sent from the future to destroy mankind.");
+            Console.WriteLine("What is ur favourite color");
+            Console.ReadLine();
+            Console.ReadKey();
+//the things put inside here are going to appear when running the program
+        }
+    }
+}
+
